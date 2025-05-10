e2447b183018b28fb1d164d3642faf08b0bc244e
shahdelwan0 <138165960+shahdelwan0@users.noreply.github.com>
2025-05-10 23:23:51 +0300

    Add Template and Nav components with CSS modules

---
+++ Front-end/src/Components/Nav/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Nav.module.css'
+
+const Nav = () => {
+    return (
+        <div className={styles.Nav}>
+
+        </div>
+    )
+}
+
+export default Nav
